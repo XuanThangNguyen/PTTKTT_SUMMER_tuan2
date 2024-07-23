d0d3bc70e8c5605c272b85a829721472d48f7006
XuanThangNguyen <thangprozzz123@gmail.com>
2024-07-24 05:07:40 +0700

    Tuan2

---
+++ ConsoleApplication1/ConsoleApplication1/MergeSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class MergeSort
+    {
+
+        public static void Merge_Sort(int[] A, int left, int right)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+                Merge_Sort(A, left, mid);
+                Merge_Sort(A, mid + 1, right);
+                Merge(A, left, mid, right);
+            }
+        }
+        static void Merge(int[] A, int left, int mid, int right)
+        {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int i = 0; i < n1; i++)
+                L[i] = A[left + i];
+
+            for (int j = 0; j < n2; j++)
+                R[j] = A[mid + j + 1];
+
+            int k = left;
+            int iL = 0;
+            int jR = 0;
+
+            while (iL < n1 && jR < n2)
+            {
+                if (L[iL] <= R[jR])
+                {
+                    A[k] = L[iL];
+                    iL++;
+                }
+                else
+                {
+                    A[k] = R[jR];
+                    jR++;
+                }
+                k++;
+            }
+
+            while (iL < n1)
+            {
+                A[k] = L[iL];
+                iL++;
+                k++;
+            }
+
+            while (jR < n2)
+            {
+                A[k] = R[jR];
+                jR++;
+                k++;
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+
+            Console.WriteLine("BinarySearch");
+            int[] array = { 2, 4, 7, 10, 13, 15, 18, 20 };
+            int key = 7;
+            int index = BinarySearch.Binary_Search(array, array.Length, key);
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+
+            }
+            Console.WriteLine("");
+            if (index != -1)
+                Console.WriteLine("Key found at index: " + index);
+            else
+                Console.WriteLine("Key not found in the array.");
+
+            Console.WriteLine("\nMergeSort");
+            int[] array2 = { 20, 15, 10, 9, 4, 2 };
+            Console.WriteLine("Array before sorting:");
+            foreach (int element in array2)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("\n");
+
+            MergeSort.Merge_Sort(array2, 0, array2.Length - 1);
+
+            Console.WriteLine("Array after sorting:");
+            foreach (int element in array2)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.WriteLine("\n\n Bài toán nhân 2 số nguyên lớn");
+
+            long X = 123;
+            long Y = 987;
+            int N = 2;
+            long result = Nhan2SoNguyenLon.Big_Number_Multi(X, Y, N);
+            Console.WriteLine($"Kết quả của {X} * {Y} là {result}");
+
+            Console.ReadKey(); 
+        }
+    }
+}
